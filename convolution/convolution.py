7768dae24a2d60585cd4e85d8a8147101158715f
satabios <max.satabiossathya@gmail.com>
2024-11-06 10:12:02 -0800

    added bias to custom conv2d pytorch implementation

--- convolution/convolution.py
+++ convolution/convolution.py
@@ -2,12 +2,9 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-# Define padding and stride globally
-padding = 0
-stride = 1
+
 
 def numpy_conv2d(input_data, kernel_weights, padding=0, stride=1):
-    # Apply padding to input data
     if padding > 0:
         input_data = np.pad(input_data, 
                             ((0, 0), (0, 0), (padding, padding), (padding, padding)), 
@@ -24,16 +21,17 @@ def numpy_conv2d(input_data, kernel_weights, padding=0, stride=1):
     output_data = np.zeros((batch_size, output_channels, output_height, output_width))
 
     for output_channel in range(output_channels):
+        kernel_data = kernel_weights[output_channel, :, :, :]
         for height in range(output_height):
             for width in range(output_width):
                 region = input_data[:, :, height * stride:height * stride + kernel_height,
                                     width * stride:width * stride + kernel_width]
                 output_data[:, output_channel, height, width] = np.sum(
-                    region * kernel_weights[output_channel, :, :, :], axis=(1, 2, 3)
+                    region * kernel_data, axis=(1, 2, 3)
                 )
     return output_data
 
-def pytorch_conv2d(input_data, kernel_weights, padding=0, stride=1):
+def pytorch_conv2d(input_data, kernel_weights, kernel_bias, padding=0, stride=1):
     if padding > 0:
         input_data = F.pad(input_data, (padding, padding, padding, padding), "constant", 0)
 
@@ -56,33 +54,53 @@ def pytorch_conv2d(input_data, kernel_weights, padding=0, stride=1):
                 output_data[:, out_channel, height, width] = torch.sum(
                     region * kernel_data, dim=(1, 2, 3)
                 )
-    return output_data
+            
+    return output_data + kernel_bias.unsqueeze(0).unsqueeze(-1).unsqueeze(-1) # Expanding along Batch, Height and Width
+
 
 
 
 torch.manual_seed(4123)
+
 if __name__ == "__main__":
     torcher = 1
     numpy = 0
     cuda = 0
 
+    conv2d = 1
+    linear = 0
+    softmax = 0
+    relu = 0
+    maxpool2d = 0
+
     if torcher:
-        input_data = torch.rand(16, 3, 32, 32).float()
-        kernel_weights = torch.rand(8, 3, 3, 3).float()
+        if(conv2d):
+            input_data = torch.rand(16, 3, 32, 32)
+            kernel_weights = torch.rand(8, 3, 3, 3)
 
-        data_out = pytorch_conv2d(input_data, kernel_weights, padding=padding, stride=stride)
+            padding = 0
+            stride = 1
 
-        torch_kernel = torch.nn.Conv2d(3, 8, 3, padding=padding, stride=stride)
-        torch_kernel.weight.data = kernel_weights
-        torch_out = torch_kernel(input_data)
+            torch_kernel = torch.nn.Conv2d(3, 8, 3, padding=padding, stride=stride)
+            torch_kernel.weight.data = kernel_weights
+            torch_out = torch_kernel(input_data)
 
-        print("Custom PyTorch Conv Output Shape:", data_out.shape)
-        print("Torch Built-in Conv Output Shape:", torch_out.shape)
-        print("Close Match:", torch.allclose(torch_out, data_out, atol=1e-2))
+            kernel_bias = torch_kernel.bias
+            data_out = pytorch_conv2d(input_data, kernel_weights, kernel_bias, padding=padding, stride=stride)
+
+            
+            print("Custom PyTorch Conv Output Shape:", data_out.shape)
+            print("Torch Built-in Conv Output Shape:", torch_out.shape)
+            print("Close Match:", torch.allclose(torch_out, data_out, atol=1e-3))
 
 
     elif numpy:
-        input_data = np.random.rand(16, 3, 32, 32)
-        kernel_weights = np.random.rand(8, 3, 3, 3)
-        data_out = numpy_conv2d(input_data, kernel_weights, padding=padding, stride=stride)
-        print("Numpy Conv Output Shape:", data_out.shape)
+        if(conv2d):
+            input_data = np.random.rand(16, 3, 32, 32)
+            kernel_weights = np.random.rand(8, 3, 3, 3)
+
+            padding = 0
+            stride = 1
+
+            data_out = numpy_conv2d(input_data, kernel_weights, padding=padding, stride=stride)
+            print("Numpy Conv Output Shape:", data_out.shape)
